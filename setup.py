8ece860ef6952cc475597ba63ef6aa1b07caf969
Ashim Paudel <paudelashim111@gmail.com>
2022-10-31 10:17:29 +0545

    Version 0.0.2 release on pypi

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="beamframe", 
-    version="0.0.1",  
+    version="0.0.2",  
     description="A python module to solve and analyse determinate 2d Beams.", 
     long_description=long_description, 
     long_description_content_type="text/markdown", 
